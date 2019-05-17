8afbc523de3b9cedfcef494512c52cd364bb01bd
alecsmirnov <alecsmirnov93@gmail.com>
2019-05-17 17:52:15 +0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,78 @@
+﻿#include <stdio.h>
+
+#include "vector.h"
+
+typedef const char* CPCHAR;
+declareVectorType(int)
+declareVectorType(char)
+declareVectorType(CPCHAR)
+
+int main(int argc, char* argv[]) {
+	vector_int* V_int = (vector_int*)malloc(sizeof(vector_int));
+	initVector_int(V_int, 0);
+	printf("Vector_int size: %d, capacity: %d\n", V_int->size, V_int->capacity);
+
+	pushBackVector_int(V_int, 9);
+	pushBackVector_int(V_int, 8);
+	pushBackVector_int(V_int, 7);
+	pushBackVector_int(V_int, 6);
+	pushBackVector_int(V_int, 5);
+	pushBackVector_int(V_int, 4);
+	pushBackVector_int(V_int, 3);
+	pushBackVector_int(V_int, 2);
+	pushBackVector_int(V_int, 1);
+	pushBackVector_int(V_int, 0);
+
+	for (size_t i = 0; i != V_int->size; ++i)
+		printf("%d ", V_int->data[i]);
+	puts("");
+
+	printf("Vector_int size: %d, capacity: %d\n\n", V_int->size, V_int->capacity);
+
+	freeVector_int(V_int);
+	free(V_int);
+	//****************************************************************************
+
+	vector_char V_char;
+	initVector_char(&V_char, 0);
+	printf("Vector_char size: %d, capacity: %d\n", V_char.size, V_char.capacity);
+
+	pushBackVector_char(&V_char, 'a');
+	pushBackVector_char(&V_char, 'B');
+	pushBackVector_char(&V_char, 'b');
+	pushBackVector_char(&V_char, 'C');
+	pushBackVector_char(&V_char, 'c');
+	pushBackVector_char(&V_char, 'C');
+	pushBackVector_char(&V_char, 'd');
+
+	for (size_t i = 0; i != V_char.size; ++i)
+		printf("%c ", V_char.data[i]);
+	puts("");
+
+	printf("Vector_char size: %d, capacity: %d\n\n", V_char.size, V_char.capacity);
+
+	freeVector_char(&V_char);
+	//****************************************************************************
+
+	vector_CPCHAR V_cpchar;
+	initVector_CPCHAR(&V_cpchar, 6);
+	
+	printf("Vector_CPCHAR size: %d, capacity: %d\n", V_cpchar.size, V_cpchar.capacity);
+
+	pushBackVector_CPCHAR(&V_cpchar, "This");
+	pushBackVector_CPCHAR(&V_cpchar, "right");
+	pushBackVector_CPCHAR(&V_cpchar, "here");
+	pushBackVector_CPCHAR(&V_cpchar, "is");
+	pushBackVector_CPCHAR(&V_cpchar, "my");
+	pushBackVector_CPCHAR(&V_cpchar, "swag!");
+
+	for (size_t i = 0; i != V_cpchar.size; ++i)
+		printf("%s ", V_cpchar.data[i]);
+	puts("");
+
+	printf("Vector_CPCHAR size: %d, capacity: %d\n", V_cpchar.size, V_cpchar.capacity);
+
+	freeVector_CPCHAR(&V_cpchar);
+
+	return 0;
+}
---
+++ vector.h
@@ -0,0 +1,63 @@
+﻿#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <stdio.h>
+#include <malloc.h>
+
+/* C++ std::vector standard Grown factor */
+#define GROWN_FACTOR 1.5					
+
+/* vector typename creator: */
+#define createType(type) vector##_##type	
+
+
+/* Vector declaration keyword */
+#define declareVectorType(type)	typedef struct createType(type) {				\
+	type* data;																	\
+																				\
+	size_t size;																\
+	size_t capacity;															\
+} createType(type);																\
+																				\
+/* Vector initialization method */												\
+void initVector_##type(createType(type)* V, size_t capacity) {					\
+	V->size = 0;																\
+	V->capacity = capacity;														\
+																				\
+	V->data = V->capacity ? (type*)malloc(sizeof(type) * V->capacity) : NULL;	\
+}																				\
+																				\
+/* Vector free method */														\
+void freeVector_##type(createType(type)* V) {									\
+	V->size = V->capacity = 0;													\
+																				\
+	free(V->data);																\
+	V->data = NULL;																\
+}																				\
+																				\
+/* Vector push back method */													\
+void pushBackVector_##type(createType(type)* V, type value) {					\
+	if (V->size == V->capacity) {												\
+		V->capacity = (size_t)(V->capacity * GROWN_FACTOR);						\
+																				\
+		if (V->size == V->capacity)												\
+			++V->capacity;														\
+																				\
+		V->data = (type*)realloc(V->data, sizeof(type) * V->capacity);			\
+	}																			\
+																				\
+	V->data[V->size++] = value;													\
+}																				\
+																				\
+/* Vector resize method */														\
+void resizeVector_##type(createType(type)* V, size_t capacity) {				\
+	if (0 < capacity) {															\
+		V->capacity = capacity;													\
+																				\
+		V->data = (type*)realloc(V->data, sizeof(type) * V->capacity);			\
+	}																			\
+	else																		\
+		freeVector_##type(V);													\
+}
+
+#endif
